ae03775a0a0dbf1badd5d9f17299c09ce7046fbd
Oskar Kubicki <s19732@pjwstk.edu.pl>
2020-03-05 16:57:54 +0100

    message

---
+++ cw1/cw1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace cw1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var website = args[0];
+
+
+
+HttpClient httpClient = new HttpClient();
+
+            var response = await httpClient.GetAsync(website);
+            if (response.IsSuccessStatusCode)
+            {
+
+                var content = await response.Content.ReadAsStringAsync();
+
+
+                var regex = new Regex("[a-z0-9]+[a-z0-9]*@[a-z]+\\.[a-z]+", RegexOptions.IgnoreCase);
+
+
+                var matches = regex.Matches(content);
+
+
+                foreach (var match in matches)
+                {
+                    Console.WriteLine(match);
+                }
+                
+
+            }
+            Console.ReadKey();
+
+
+            
+
+        }
+
+
+
+       
+    }
+}
